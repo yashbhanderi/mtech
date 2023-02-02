4b72918fda9d7bc3f5709b9005209fbe092475db
Yash Bhanderi <yash.bhanderi@marutitech.com>
2023-02-02 18:51:41 +0530

    2_Feb

---
+++ Loops.cs
@@ -0,0 +1,55 @@
+// using System;
+ 
+// namespace mtech
+ 
+// {
+//     class Loops {
+ 
+//        static void Main(string[] args)  {
+            
+//             // While loop
+
+//             // int x = 1;
+//             // while(x <= 10) {
+//             //     Console.Write(x*10 + ", ");
+//             //     x++;
+//             // }
+
+
+//             // do while loop
+
+//             // int x = 10;
+//             // do {
+//             //     Console.Write(x*10 + ", ");
+//             //     x--;
+//             // } while(x>=1);
+
+
+//             // For Loop
+
+//             // for(int i=1; i<=10; i++) {
+//             //     Console.Write(i*10 + ", ");
+//             // }
+
+
+//             // foreach loop
+
+//             int[] arr = new int[] {1,2,3};
+
+//             foreach(int i in arr) {
+//                 Console.WriteLine(i);
+//             }
+//             foreach(dynamic i in arr) {         // var can also work
+//                 Console.WriteLine(i);
+//             }
+            
+//             // Limitations
+//             // Can't modify value during traversal
+//             // Can't get index of element
+//             // Can't go back
+
+
+//         }
+ 
+//     }
+// }
