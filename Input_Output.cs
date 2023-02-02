555b067186fb4a13a1fbdfde762f06b684b67d40
Yash Bhanderi <yash.bhanderi@marutitech.com>
2023-02-02 15:35:23 +0530

    2_feb

---
+++ Input_Output.cs
@@ -0,0 +1,25 @@
+// using System;
+ 
+// namespace mtech
+ 
+// {
+//     class Input_Output {
+ 
+//        static void Main(string[] args)  {
+
+//             // WriteLine = New line
+//             // Write = No new line
+//             Console.WriteLine("Enter the value of x : ");            
+
+//             // ReadLine can only read string value
+//             // We have to convert to int during Runtime like Python
+//             int x = Convert.ToInt32(Console.ReadLine());
+
+//             // 2 options
+//             // Console.WriteLine($"Value of x is {x}")
+//             // Console.WriteLine("Value of x is " + x)
+//             Console.WriteLine("Value of x is " + x);
+//         }
+ 
+//     }
+// }
---
+++ Variables.cs
@@ -0,0 +1,28 @@
+using System;
+ 
+namespace mtech
+ 
+{
+    class Variables {
+ 
+       static void Main(string[] args)  {
+            
+            // Local Variables
+            // static / class variables
+            // instance variables ( Non static variables )
+            // const variables
+
+            // 5) readonly
+            // same like const...But can intitalize after declaration in the constructor
+            // Can't modified after initialization
+
+            // 6) implicitly typed variables
+
+            // var x = 10;
+            // Console.WriteLine("Type of x is : " + x.GetType());
+
+            // But main thing... it can only use as local scope variables
+        }
+ 
+    }
+}
