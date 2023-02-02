36009be9cbdbe33c44ef623e2ec89e2dc147ca5c
Yash Bhanderi <yash.bhanderi@marutitech.com>
2023-02-02 15:37:19 +0530

    2_feb

--- Input_Output.cs
+++ Input_Output.cs
@@ -1,25 +1,25 @@
-// using System;
+using System;
  
-// namespace mtech
+namespace mtech
  
-// {
-//     class Input_Output {
+{
+    class Input_Output {
  
-//        static void Main(string[] args)  {
+       static void Main(string[] args)  {
 
-//             // WriteLine = New line
-//             // Write = No new line
-//             Console.WriteLine("Enter the value of x : ");            
+            // WriteLine = New line
+            // Write = No new line
+            Console.WriteLine("Enter the value of x : ");            
 
-//             // ReadLine can only read string value
-//             // We have to convert to int during Runtime like Python
-//             int x = Convert.ToInt32(Console.ReadLine());
+            // ReadLine can only read string value
+            // We have to convert to int during Runtime like Python
+            int x = Convert.ToInt32(Console.ReadLine());
 
-//             // 2 options
-//             // Console.WriteLine($"Value of x is {x}")
-//             // Console.WriteLine("Value of x is " + x)
-//             Console.WriteLine("Value of x is " + x);
-//         }
+            // 2 options
+            // Console.WriteLine($"Value of x is {x}")
+            // Console.WriteLine("Value of x is " + x)
+            Console.WriteLine("Value of x is " + x);
+        }
  
-//     }
-// }
+    }
+}
--- Variables.cs
+++ Variables.cs
@@ -7,21 +7,21 @@ namespace mtech
  
        static void Main(string[] args)  {
             
-            // Local Variables
-            // static / class variables
-            // instance variables ( Non static variables )
-            // const variables
+            Local Variables
+            static / class variables
+            instance variables ( Non static variables )
+            const variables
 
-            // 5) readonly
-            // same like const...But can intitalize after declaration in the constructor
-            // Can't modified after initialization
+            5) readonly
+            same like const...But can intitalize after declaration in the constructor
+            Can't modified after initialization
 
-            // 6) implicitly typed variables
+            6) implicitly typed variables
 
-            // var x = 10;
-            // Console.WriteLine("Type of x is : " + x.GetType());
+            var x = 10;
+            Console.WriteLine("Type of x is : " + x.GetType());
 
-            // But main thing... it can only use as local scope variables
+            But main thing... it can only use as local scope variables
         }
  
     }
